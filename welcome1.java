b7393f230e4209c40366afd573f0ceff58f91ce8
Mananjay Mishra <mananmis@in.ibm.com>
2020-04-29 15:00:32 +0530

    added two files

---
+++ welcome1.java
@@ -0,0 +1,23 @@
+//Mananjay added
+//firstbranch modified
+
+package com.ibm.pes.dlo.cmn;
+
+import com.ibm.pes.dlo.KeyedDLO;
+public interface PrCostCenter extends KeyedDLO{
+	
+	CostCenterKey getCostCenterKey();
+	
+	String getApproverWebId();
+	
+	String getDivisionCd();
+	
+	String getLedgerCd();
+	
+	String getLedgerReportingUnit();
+	
+	String getAccountMajor();
+	
+	boolean isBlocked();	
+}//EndInterface
+//Added last line by Mananjay
