776174a329df0ee56d7594000b8112f839b3eea8
Mananjay Mishra <mananmis@in.ibm.com>
2020-04-29 10:17:58 +0530

    Commit the code of cost and costfactory file

---
+++ Cost.java
@@ -0,0 +1,20 @@
+
+package com.ibm.pes.dlo.cmn;
+
+import com.ibm.pes.dlo.KeyedDLO;
+public interface PrCostCenter extends KeyedDLO{
+	
+	CostCenterKey getCostCenterKey();
+	
+	String getApproverWebId();
+	
+	String getDivisionCd();
+	
+	String getLedgerCd();
+	
+	String getLedgerReportingUnit();
+	
+	String getAccountMajor();
+	
+	boolean isBlocked();	
+}//EndInterface
---
+++ Costfactory.java
@@ -0,0 +1,17 @@
+
+package com.ibm.pes.dlo.cmn;
+
+import com.ibm.pes.dlo.DLOProxyNamer;
+import com.ibm.pes.exception.ApplicationException;
+import com.ibm.pes.fwutil.Instantiater;
+import com.ibm.pes.logging.LogMsg;
+
+/**
+ * <code>PrCostCenterFactory</code> produces CostCenter objects. 
+ */
+public abstract class PrCostCenterFactory{
+	private static PrCostCenterFactory soleInstance = null;
+	
+	
+	public abstract PrCostCenter produce(CostCenterKey ccKey) throws ApplicationException;
+}//EndClass
